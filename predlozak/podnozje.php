5bbefb3fcf59325014e2d32aa31277255e0361cf
Strahinja Negovanović <strahinja944@gmail.com>
2016-06-29 23:32:10 +0200

    Rebuilt

--- predlozak/podnozje.php
+++
@@ -1,6 +0,0 @@
-	<footer class="footer">
-		      	<p>© 2016 Strahinja Negovanović</p>
-		      	<a href="https://www.facebook.com/strahinja.negovanovic"><img title="Facebook" src="ikonice/facebook.png" width="35px" height="35px"/></a>
-				<a href="https://twitter.com/Kistras"><img title="Twitter" src="ikonice/twitter.png" width="35px" height="35px"/></a>
-				<a href="https://plus.google.com/u/0/103167825392726927406/posts"><img title="Google" src="ikonice/google.png" width="35px" height="35px"/></a>
-    </footer>
